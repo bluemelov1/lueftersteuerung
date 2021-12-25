9c75467ee7e6da1437819986d77ca615a019325d
bluemelov <hannesblumel44@gmail.com>
2021-12-25 14:05:24 +0100

    favicon hinzugefügt (functioniert nicht?!)

--- website+steuerung/public/js/ajax.js
+++ website+steuerung/public/js/ajax.js
@@ -1,5 +1,24 @@
 // Asynchrone Ajax serveranfragen und befehle
+let temp_haus;
+let temp_wintergarten;
+let min_wintergarten;
+let max_wohnraum;
+let differenz;
+let automatik;
 
+
+
+//Initiale Daten Abfragen 
+window.onload = function () {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function (){
+        let json = JSON.parse(this.response);
+        console.log(json);
+    }
+    xhttp.open("GET", "data");
+    xhttp.send();
+    
+}
 
 
 //Modi: Automatik und Manuell
